11709e1278e06f0c5c8a1d10cb5fec727ef69d2d
Edmond120 <edmondwongny@gmail.com>
2024-01-08 17:17:43 -0500

    Implement command: mutant create

--- mutant/cli/commands/create.py
+++ mutant/cli/commands/create.py
@@ -1,2 +1,11 @@
+from sys import stderr
+from pathlib import Path
+from mutant.directory.manager import MutantDirectory
+
 def main(args):
-	return 'Test output'
+	directory = Path(args.directory)
+	if directory.exists():
+		print(f'Error: file({args.directory}) exists', file=stderr)
+		return 1
+	mutant_dir = MutantDirectory.create(directory)
+	return 0
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,5 @@
+import pytest
+from pathlib import Path
 from mutant.cli.parser import main_parser
 from mutant.cli import run_command
 from mutant.cli import commands
@@ -8,4 +10,9 @@ class TestCli:
 		assert args.command_name == 'create'
 		assert args.command_func == commands.create
 		assert args.directory == 'dotfiles'
-		assert run_command(['create', 'dotfiles']) == 'Test output'
+
+	def test_command_create(self, tmp_path, monkeypatch):
+		monkeypatch.chdir(tmp_path)
+		exit_code = run_command(['create', 'dotfiles'])
+		assert exit_code == 0
+		assert Path('dotfiles').is_dir()
