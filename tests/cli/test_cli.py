b93c92629cf5b385e8959496e72e40ce3eaad825
Edmond120 <edmondwongny@gmail.com>
2024-01-08 18:00:15 -0500

    Move cli test file

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -5,7 +5,7 @@ from mutant.cli import run_command
 from mutant.cli import commands
 
 class TestCli:
-	def test_parser(self):
+	def test_main_parser(self):
 		args = main_parser.parse_args(['create', 'dotfiles'])
 		assert args.command_name == 'create'
 		assert args.command_func == commands.create
